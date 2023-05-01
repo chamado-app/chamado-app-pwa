b931cf33209ac41a3ea35437c0d6ea55776d54c4
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-05-01 19:06:32 -0300

    refactor: change the type strategy of the rendering function in the tests

--- src/components/ui/col/q-col.spec.ts
+++ src/components/ui/col/q-col.spec.ts
@@ -1,14 +1,11 @@
-import {
-  type RenderOptions,
-  type RenderResult,
-  render
-} from '@testing-library/vue'
+import { type RenderResult, render } from '@testing-library/vue'
 
-import { Col } from '.'
-import type { QColProps } from '.'
+import { type RenderOptions } from '@/tests/types'
 
-const makeSut = ({ props, slots }: RenderOptions = {}): RenderResult =>
-  render(Col, { props, slots })
+import { Col, type QColProps } from '.'
+
+const makeRender = (options: RenderOptions<QColProps> = {}): RenderResult =>
+  render(Col, options)
 
 const getElement = (container: Element): Element | null =>
   container.querySelector('div > div')
@@ -17,13 +14,13 @@ describe('Components/Ui/Col', () => {
   test('should render with correct slot', () => {
     const slotValue = 'any_slot_value'
 
-    const { getByText } = makeSut({ slots: { default: slotValue } })
+    const { getByText } = makeRender({ slots: { default: slotValue } })
 
     expect(getByText(slotValue)).toBeInTheDocument()
   })
 
   test('should render only with "col-12" class if no prop size is provided', () => {
-    const { container } = makeSut()
+    const { container } = makeRender()
 
     expect(getElement(container)).toHaveClass('col-12')
   })
@@ -31,7 +28,7 @@ describe('Components/Ui/Col', () => {
   test('should render with "col-12" and "sm" classes if it is provided', () => {
     const props: QColProps = { sm: '6' }
 
-    const { container } = makeSut({ props })
+    const { container } = makeRender({ props })
 
     expect(getElement(container)).toHaveClass('col-12', `col-sm-${props.sm}`)
   })
@@ -39,7 +36,7 @@ describe('Components/Ui/Col', () => {
   test('should render with "col-12" and "md" classes if it is provided', () => {
     const props: QColProps = { md: '4' }
 
-    const { container } = makeSut({ props })
+    const { container } = makeRender({ props })
 
     expect(getElement(container)).toHaveClass('col-12', `col-md-${props.md}`)
   })
@@ -47,14 +44,14 @@ describe('Components/Ui/Col', () => {
   test('should render with "col-12" and "lg" classes if it is provided', () => {
     const props: QColProps = { lg: '3' }
 
-    const { container } = makeSut({ props })
+    const { container } = makeRender({ props })
 
     expect(getElement(container)).toHaveClass('col-12', `col-lg-${props.lg}`)
   })
 
   test('should render with "col-12" and "xl" classes if it is provided', () => {
     const props: QColProps = { xl: '2' }
-    const { container } = makeSut({ props })
+    const { container } = makeRender({ props })
 
     expect(getElement(container)).toHaveClass('col-12', `col-xl-${props.xl}`)
   })
@@ -62,7 +59,7 @@ describe('Components/Ui/Col', () => {
   test('should render with all classes if it is provided', () => {
     const props: QColProps = { sm: '6', md: '4', lg: '3', xl: '2' }
 
-    const { container } = makeSut({ props })
+    const { container } = makeRender({ props })
 
     expect(getElement(container)).toHaveClass(
       'col-12',
--- src/components/ui/row/q-row.spec.ts
+++ src/components/ui/row/q-row.spec.ts
@@ -1,14 +1,11 @@
-import {
-  type RenderOptions,
-  type RenderResult,
-  render
-} from '@testing-library/vue'
+import { type RenderResult, render } from '@testing-library/vue'
 
-import { Row } from '.'
-import type { QRowProps } from '.'
+import type { RenderOptions } from '@/tests/types'
 
-const makeSut = ({ props, slots }: RenderOptions = {}): RenderResult =>
-  render(Row, { props, slots })
+import { type QRowProps, Row } from '.'
+
+const makeRender = (options: RenderOptions<QRowProps> = {}): RenderResult =>
+  render(Row, options)
 
 const getElement = (container: Element): Element | null =>
   container.querySelector('.row > div > div')
@@ -17,13 +14,13 @@ describe('Components/Ui/Row', () => {
   test('should render with correct slot', () => {
     const slotValue = 'any_slot_value'
 
-    const { getByText } = makeSut({ slots: { default: slotValue } })
+    const { getByText } = makeRender({ slots: { default: slotValue } })
 
     expect(getByText(slotValue)).toBeInTheDocument()
   })
 
   test('should render with "md" gutter by default prop', () => {
-    const { container } = makeSut()
+    const { container } = makeRender()
 
     expect(getElement(container)).toHaveClass('row', 'q-col-gutter-md')
   })
@@ -31,7 +28,7 @@ describe('Components/Ui/Row', () => {
   test('should render with correct prop gutter', () => {
     const props: QRowProps = { gutter: 'lg' }
 
-    const { container } = makeSut({ props })
+    const { container } = makeRender({ props })
 
     expect(getElement(container)).toHaveClass(
       'row',
