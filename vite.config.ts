dbc48b5a71e8d9271fb5c79c023e540f45f155fd
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-05-05 21:08:26 -0300

    chore: add more vitest configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 
 import { URL, fileURLToPath } from 'node:url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
@@ -17,26 +16,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  test: {
-    globals: true,
-    setupFiles: ['src/test-utils/setup/vitest.ts'],
-    coverage: {
-      provider: 'istanbul',
-      all: true,
-      cleanOnRerun: true,
-      statements: 90,
-      branches: 90,
-      reporter: ['text', 'html'],
-      include: ['src/**/*.{vue,ts}'],
-      exclude: [
-        '**/*.{d,spec}.ts',
-        'src/**/{router,stores,assets,layouts,setup,test-utils}/**',
-        'src/**/{types,index}.ts',
-        'src/App.vue',
-        'src/main.ts'
-      ]
     }
   }
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,25 @@ export default mergeConfig(
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./', import.meta.url))
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      globals: true,
+      setupFiles: ['src/test-utils/setup/vitest.ts'],
+      coverage: {
+        provider: 'istanbul',
+        all: true,
+        cleanOnRerun: true,
+        statements: 90,
+        branches: 90,
+        reporter: ['text', 'lcov', 'html-spa'],
+        include: ['src/**/*.{vue,ts}'],
+        exclude: [
+          '**/*.{d,spec}.ts',
+          'src/**/{router,stores,assets,layouts,setup,test-utils}/**',
+          'src/**/{types,index}.ts',
+          'src/App.vue',
+          'src/main.ts'
+        ]
+      }
     }
   })
 )
