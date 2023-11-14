71c8dab185c1fc99c6cd5c27eca540266305bd62
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-11-14 19:22:16 -0300

    feat: create usecase to logout

--- src/data/usecases/auth/index.ts
+++ src/data/usecases/auth/index.ts
@@ -1,3 +1,4 @@
 export * from './cookie-store-auth-token'
 export * from './remote-authenticate'
+export * from './remote-logout'
 export * from './remote-whoami'
--- src/domain/usecases/auth/index.ts
+++ src/domain/usecases/auth/index.ts
@@ -1,3 +1,4 @@
 export * from './authenticate.usecase'
+export * from './logout.usecase'
 export * from './store-auth-token.usecase'
 export * from './whoami.usecase'
